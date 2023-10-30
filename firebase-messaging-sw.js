0e635f8339cdef05cfdb56654a04c60ad299e1c3
cupidjhj <128975691+cupidjhj@users.noreply.github.com>
2023-10-30 12:17:09 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,42 +8,42 @@ self.addEventListener("activate", function (e) {
 });
 
 self.addEventListener("push", function (e) {
-  const permission = Notification.requestPermission();
-  if (permission === "denied") {
-    console.log("Alarm Denied");
-    return;
-  }
-  console.log("Alarm Granted");
-  console.log("push: ", e.data.json());
-  messaging.setBackgroundMessageHandler(function(payload) {
-    const notificationTitle = 'New Message 💘';
-    const notificationOptions = {
-      body: payload.data.message,
-    };
+  // const permission = Notification.requestPermission();
+  // if (permission === "denied") {
+  //   console.log("Alarm Denied");
+  //   return;
+  // }
+  // console.log("Alarm Granted");
+  // console.log("push: ", e.data.json());
+  // messaging.setBackgroundMessageHandler(function(payload) {
+  //   const notificationTitle = '';
+  //   const notificationOptions = {
+  //     body: payload.data.message,
+  //   };
   
-    return self.registration.showNotification(notificationTitle, notificationOptions);
-  });
+  //   return self.registration.showNotification(notificationTitle, notificationOptions);
+  // });
 });
 
 self.addEventListener("notificationclick", function (event) {
-  console.log("notification click");
-  const url = "/";
-  event.notification.close();
-  event.waitUntil(clients.openWindow(url));
+  // console.log("notification click");
+  // const url = "/";
+  // event.notification.close();
+  // event.waitUntil(clients.openWindow(url));
 });
 
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCucNljgWlnQc7uWT-UdGvinCo28jRwIuw",
-  authDomain: "jhj-chats.firebaseapp.com",
-  projectId: "jhj-chats",
-  storageBucket: "jhj-chats.appspot.com",
-  messagingSenderId: "968225357706",
-  appId: "1:968225357706:web:b3dece82d8a2115a99d273",
-  measurementId: "G-Q7PE4SZQ00"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCucNljgWlnQc7uWT-UdGvinCo28jRwIuw",
+//   authDomain: "jhj-chats.firebaseapp.com",
+//   projectId: "jhj-chats",
+//   storageBucket: "jhj-chats.appspot.com",
+//   messagingSenderId: "968225357706",
+//   appId: "1:968225357706:web:b3dece82d8a2115a99d273",
+//   measurementId: "G-Q7PE4SZQ00"
+// };
 
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+// firebase.initializeApp(firebaseConfig);
+// const messaging = firebase.messaging();
